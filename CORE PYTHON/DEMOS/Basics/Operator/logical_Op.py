ac1a9046b514b92d8484a2103c400554bb8dfda3
OM DESHMANE <omdeshmane18@gmail.com>
2026-09-09 12:24:08 +0530

    Uploads all

---
+++ CORE PYTHON/DEMOS/Basics/Operator/logical_Op.py
@@ -0,0 +1,11 @@
+#1) and: If both operands are true then returns True otherwise False
+
+print(True and True)  
+
+#2) or: If both operands are False then returns false otherwise True
+
+print(False or False)
+
+#3) not: Opposite of operand
+
+print(not False)
