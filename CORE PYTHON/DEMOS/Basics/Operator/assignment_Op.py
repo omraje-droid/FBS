54577a547771077ecfe038c3519f5a7362188fcd
OM DESHMANE <omdeshmane18@gmail.com>
2026-09-10 16:41:31 +0530

    Upload all

---
+++ CORE PYTHON/DEMOS/Basics/Operator/assignment_Op.py
@@ -0,0 +1,24 @@
+num = int(input("Enter the number:"))
+a = num
+print(num)
+
+a += 1
+print (a)
+
+a -= 1
+print(a)
+
+a *= 2
+print(a)
+
+a /= 2
+print(a)
+
+a //= 2
+print(a)
+
+a %= 2
+print(a)
+
+a **= a
+print(a)
