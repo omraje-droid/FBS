035d6af6ad0239be9918ff1cf8bc733622a54773
OM DESHMANE <omdeshmane18@gmail.com>
2026-09-12 02:55:11 +0530

    Update all

---
+++ CORE PYTHON/Practice/if_else_ladder/p/n/P_N_Zero.py
@@ -0,0 +1,9 @@
+#1. Write a Python program to accept a number and check whether it is positive, negative, or zero.
+num = int(input("Enter the number:"))
+
+if(num == 0):
+    print("Given number is zero.")
+elif(num > 0):
+    print("Given number is positive.")
+else:
+    print("Givem number is negative.")
